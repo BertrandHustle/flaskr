b7b2b974ce3e341323fdd71a2082f8c1fbae3434
BertrandHustle <MFOrangeSlice@gmail.com>
2017-05-12 09:30:25 -0400

    working on getting initdb to work

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -1,6 +1,6 @@
 import os
 import sqlite3
-import flask
+from flask import Flask, current_app
 
 app = Flask(__name__) # create application instance
 app.config.from_object(__name__) # load config from this file (flaskr.py)
@@ -13,8 +13,33 @@ app.config.update(dict(
 ))
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
+# Connects to the specific DATABASE
 def connect_db():
-    # Connects to the specific DATABASE
     rv = sqlite3.connect(app.config['DATABASE'])
     rv.row_factory = sqlite3.Row
     return rv
+
+# initializes database
+def init_db():
+    db = get.db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+@app.cli.command('initdb')
+def initdb_command():
+    init_db()
+    print('Initialized database successfully')
+
+# Opens a new database connection if there already isn't one
+# for the current application context
+def get_db():
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+# Closes database at end of request
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
